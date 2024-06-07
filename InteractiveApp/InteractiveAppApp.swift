b5063036e77bf62b11a5ea8c3e4d09b9092365fb
Harini <harinichamp02@gmail.com>
2024-06-07 10:04:00 -0500

    Initial Commit

---
+++ InteractiveApp/InteractiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAppApp.swift
+//  InteractiveApp
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
